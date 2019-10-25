ebbec0c842d2b42be5b22a27bb83272636595fd2
李晨埜 <lichenye@dhcp-clara-469.eduroam.cornell.edu>
2019-10-25 00:21:42 -0400

    task 2

--- manip/src/manip/ScaleManipulator.java
+++ manip/src/manip/ScaleManipulator.java
@@ -36,7 +36,25 @@ public class ScaleManipulator extends Manipulator {
 		//   corner of the screen, and (1, 1) is the top right corner of the screen.
 
 		// A3 SOLUTION BEGIN
-
+		if (this.axis == ManipulatorAxis.X) {
+			Vector3 malOrig = new Vector3(0, 0, 0);
+			Vector3 malDir = new Vector3(1, 0, 0);
+			float t1 = this.getAxisT(malOrig, malDir, viewProjection, curMousePos);
+			float t2 = this.getAxisT(malOrig, malDir, viewProjection, lastMousePos);
+			this.reference.translation.set(0, 0, this.reference.translation.get(0, 0) * (t1/t2));
+		} else if (this.axis == ManipulatorAxis.Y) {
+			Vector3 malOrig = new Vector3(0, 0, 0);
+			Vector3 malDir = new Vector3(0, 1, 0);
+			float t1 = this.getAxisT(malOrig, malDir, viewProjection, curMousePos);
+			float t2 = this.getAxisT(malOrig, malDir, viewProjection, lastMousePos);
+			this.reference.translation.set(1, 1, this.reference.translation.get(1, 1) * (t1/t2));
+		} else if (this.axis == ManipulatorAxis.Z) {
+			Vector3 malOrig = new Vector3(0, 0, 0);
+			Vector3 malDir = new Vector3(0, 0, 1);
+			float t1 = this.getAxisT(malOrig, malDir, viewProjection, curMousePos);
+			float t2 = this.getAxisT(malOrig, malDir, viewProjection, lastMousePos);
+			this.reference.translation.set(2, 2, this.reference.translation.get(2, 2) * (t1/t2));
+		}
 		// A3 SOLUTION END
 	}
 
